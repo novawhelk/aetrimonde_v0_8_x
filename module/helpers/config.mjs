6146428f94771a1939e029f855f2928dd8b19d06
Zachary <zkiefer@uoregon.edu>
2021-07-21 23:30:33 -0700

    Introduced boilerplate system as base for expansion

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AETRIMONDE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AETRIMONDE.abilities = {
+  "str": "AETRIMONDE.AbilityStr",
+  "dex": "AETRIMONDE.AbilityDex",
+  "con": "AETRIMONDE.AbilityCon",
+  "int": "AETRIMONDE.AbilityInt",
+  "wis": "AETRIMONDE.AbilityWis",
+  "cha": "AETRIMONDE.AbilityCha"
+};
+
+AETRIMONDE.abilityAbbreviations = {
+  "str": "AETRIMONDE.AbilityStrAbbr",
+  "dex": "AETRIMONDE.AbilityDexAbbr",
+  "con": "AETRIMONDE.AbilityConAbbr",
+  "int": "AETRIMONDE.AbilityIntAbbr",
+  "wis": "AETRIMONDE.AbilityWisAbbr",
+  "cha": "AETRIMONDE.AbilityChaAbbr"
+};
