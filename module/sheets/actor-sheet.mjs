75571feaad2dfba1a9360fb5517d50ec5e844cfe
Zachary <zkiefer@uoregon.edu>
2021-07-22 19:26:39 -0700

    Update to eliminate some warning messages

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -416,7 +416,7 @@ export class AetrimondeActorSheet extends ActorSheet {
     html.find('.rollable').click(this._onRoll.bind(this));
 
     // Drag events for macros.
-    if (this.actor.owner) {
+    if (this.actor.isOwner) {
       let handler = ev => this._onDragStart(ev);
       html.find('li.item').each((i, li) => {
         if (li.classList.contains("inventory-header")) return;
